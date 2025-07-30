a09decf082779c96c81dae352cccb4f74fe16c3c
mac-dubem <nwadimkpachidubem@gmail.com>
2025-07-30 17:26:33 +0100

    rounding up with the UI

--- lib/task_text.dart
+++ lib/task_text.dart
@@ -2,25 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:todoo_app/constants.dart';
 
 class TaskText extends StatefulWidget {
-  const TaskText({super.key, required this.text});
+  const TaskText({
+    super.key,
+    required this.text,
+    // required this.taskCompleted,
+    // required this.onChanged,
+  });
 
   final String text;
+
+  // final bool taskCompleted;
+  // final bool onChanged;
 
   @override
   State<TaskText> createState() => _TaskTextState();
 }
 
 class _TaskTextState extends State<TaskText> {
-  bool isChecked = false;
+  bool taskCompleted = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: Checkbox(
-        value: isChecked,
+        value: taskCompleted,
         onChanged: (bool? value) {
           setState(() {
-            isChecked = value ?? false;
+            taskCompleted = value ?? false;
           });
         },
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
@@ -28,7 +36,11 @@ class _TaskTextState extends State<TaskText> {
       ),
       title: Text(
         widget.text,
+
         style: TextStyle(
+          decoration: taskCompleted
+              ? TextDecoration.lineThrough
+              : TextDecoration.none,
           // fontFamily: "GloriaHallelujah",
           fontSize: 20,
         ),
